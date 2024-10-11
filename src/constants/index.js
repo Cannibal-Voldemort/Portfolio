12dcc5b750afa18448198ea9a6c55dd0e0acfeaf
Aman Kumar Singh <singhkumaraman17@gmail.com>
2024-10-11 18:46:05 +0530

    Progress So Far

---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+// import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "May 2023 - July 2023",
+    role: "Front-end Developer",
+    company: "InterPe",
+    description: '',
+    technologies: ["HTML", "CSS", "Javascript", "React.js"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "tailwindcss","Javascript", "React", "Node.js","Express.js", "MongoDB"],
+  },
+  {
+    title: "MovieHub",
+    image: project2,
+    description:
+      ".",
+    technologies: ["HTML", "Tailwindcss", "JavaScript", "React", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "tailwindCss", "React", "Javascript"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "+91 9707002098",
+  email: "singhkumaraman17@gmail.com",
+};
